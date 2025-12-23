063f946f3893fb4914f53a584205cb0d8d5b3f9e
anuroopks-matrix <anuroop.sajeevan@matrixhealth.net>
2025-12-23 19:45:49 +0530

    Update .gitignore to include additional IDE configurations, build files, and environment variables

---
+++ src/test/java/com/aksgrpc/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aksgrpc.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
